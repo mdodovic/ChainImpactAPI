163b117e21a577ce98b71ee550f843b046e711e9
mdodovic <matija.dodovic@gmail.com>
2023-03-09 13:27:06 +0100

    NFT Owners

--- ChainImpactDB/insert_nftowner.sql
+++ ChainImpactDB/insert_nftowner.sql
@@ -26,13 +26,6 @@ INSERT INTO public.nftowner(
 		,(select id from impactor where name = 'RedTech')	
 	);	
 
-INSERT INTO public.nftowner(
-	nfttypeid,
-	impactorid)
-	VALUES (
-		(select id from nfttype where tier = 2 AND usertype = 0 AND causetypeid = (select id from causetype where name like 'general'))
-		,(select id from impactor where name = 'RedTech')	
-	);	
 
 -- health
 INSERT INTO public.nftowner(
@@ -59,4 +52,30 @@ INSERT INTO public.nftowner(
 		,(select id from impactor where name = 'RedTech')	
 	);	
 	
+-- disaster relief
+INSERT INTO public.nftowner(
+	nfttypeid,
+	impactorid)
+	VALUES (
+		(select id from nfttype where tier = 0 AND usertype = 0 AND causetypeid = (select id from causetype where name like 'disaster relief'))
+		,(select id from impactor where name = 'RedTech')	
+	);	
+	
+INSERT INTO public.nftowner(
+	nfttypeid,
+	impactorid)
+	VALUES (
+		(select id from nfttype where tier = 1 AND usertype = 0 AND causetypeid = (select id from causetype where name like 'disaster relief'))
+		,(select id from impactor where name = 'RedTech')	
+	);	
 
+
+-- health
+INSERT INTO public.nftowner(
+	nfttypeid,
+	impactorid)
+	VALUES (
+		(select id from nfttype where tier = 0 AND usertype = 0 AND causetypeid = (select id from causetype where name like 'education'))
+		,(select id from impactor where name = 'RedTech')	
+	);	
+	
